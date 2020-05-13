29e1e92dc9fc3e1341f1cd99c25f389e5dfdb992
Zhahira <zhahira.s.r@mail.ugm.ac.id>
2020-05-13 09:45:02 +0700

    Updates

---
+++ precache-manifest.9ae3d9958230719c33b29085054d3d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c4a97c34d8e791de0999ee5a9af5bd",
+    "url": "/to-do-notes-uas/index.html"
+  },
+  {
+    "revision": "d9b1d821f6cf9334f532",
+    "url": "/to-do-notes-uas/static/css/main.f970b54f.chunk.css"
+  },
+  {
+    "revision": "76bf2b3bee512557c5ba",
+    "url": "/to-do-notes-uas/static/js/2.476c21bb.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/to-do-notes-uas/static/js/2.476c21bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b1d821f6cf9334f532",
+    "url": "/to-do-notes-uas/static/js/main.e5256926.chunk.js"
+  },
+  {
+    "revision": "c836620adde8b1a1bf4b",
+    "url": "/to-do-notes-uas/static/js/runtime-main.0558b3ad.js"
+  }
+]);
